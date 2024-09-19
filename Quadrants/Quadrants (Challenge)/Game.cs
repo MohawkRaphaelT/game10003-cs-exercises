77c3a19a37f13eb77160847a7777deb18d9cc517
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2024-09-19 15:11:31 -0400

    m: Reduce Quadrant namespace indentation

--- Quadrants/Quadrants (Challenge)/Game.cs
+++ Quadrants/Quadrants (Challenge)/Game.cs
@@ -5,42 +5,41 @@
 using System;
 using System.Numerics;
 
-namespace Game10003
+namespace Game10003;
+
+public class Game
 {
-    public class Game
+    public void Setup()
     {
-        public void Setup()
-        {
-            Window.SetTitle("Quadrants");
-            Window.SetSize(400, 400);
-        }
+        Window.SetTitle("Quadrants");
+        Window.SetSize(400, 400);
+    }
 
-        public void Update()
-        {
-            // Clear screen
-            Window.ClearBackground(Color.OffWhite);
+    public void Update()
+    {
+        // Clear screen
+        Window.ClearBackground(Color.OffWhite);
 
-            // Top-Left
-            Draw.FillColor = Color.Red;
-            Draw.Square(0, 0, 200);
+        // Top-Left
+        Draw.FillColor = Color.Red;
+        Draw.Square(0, 0, 200);
 
-            // Top-Right
-            Draw.FillColor = Color.Green;
-            Draw.Square(200, 0, 200);
+        // Top-Right
+        Draw.FillColor = Color.Green;
+        Draw.Square(200, 0, 200);
 
-            // Bottom-Left
-            Draw.FillColor = Color.Yellow;
-            Draw.Square(0, 200, 200);
+        // Bottom-Left
+        Draw.FillColor = Color.Yellow;
+        Draw.Square(0, 200, 200);
 
-            // Bottom-Right
-            Draw.FillColor = Color.Blue;
-            Draw.Square(200, 200, 200);
+        // Bottom-Right
+        Draw.FillColor = Color.Blue;
+        Draw.Square(200, 200, 200);
 
-            // Draw lines to separate screen into quadrants
-            Draw.LineSize = 2;
-            Draw.LineSharp(Window.Width / 2, 0, Window.Width / 2, Window.Height);
-            Draw.LineSharp(0, Window.Height / 2, Window.Width, Window.Height / 2);
-            Draw.LineSize = 0;
-        }
+        // Draw lines to separate screen into quadrants
+        Draw.LineSize = 2;
+        Draw.LineSharp(Window.Width / 2, 0, Window.Width / 2, Window.Height);
+        Draw.LineSharp(0, Window.Height / 2, Window.Width, Window.Height / 2);
+        Draw.LineSize = 0;
     }
 }
--- Quadrants/Quadrants (Solution)/Game.cs
+++ Quadrants/Quadrants (Solution)/Game.cs
@@ -5,59 +5,58 @@
 using System;
 using System.Numerics;
 
-namespace Game10003
+namespace Game10003;
+
+public class Game
 {
-    public class Game
+    public void Setup()
     {
-        public void Setup()
+        Window.SetTitle("Quadrants");
+        Window.SetSize(400, 400);
+    }
+
+    public void Update()
+    {
+        // Clear screen
+        Window.ClearBackground(Color.OffWhite);
+
+        float mouseX = Input.GetMouseX();
+        float mouseY = Input.GetMouseY();
+        float halfWindowW = Window.Width / 2;
+        float halfWindowH = Window.Height / 2;
+
+        // Top-Left
+        if (mouseX < halfWindowW && mouseY < halfWindowH)
         {
-            Window.SetTitle("Quadrants");
-            Window.SetSize(400, 400);
+            Draw.FillColor = Color.Red;
+            Draw.Square(0, 0, 200);
         }
 
-        public void Update()
+        // Top-Right
+        if (mouseX > halfWindowW && mouseY < halfWindowH)
         {
-            // Clear screen
-            Window.ClearBackground(Color.OffWhite);
-
-            float mouseX = Input.GetMouseX();
-            float mouseY = Input.GetMouseY();
-            float halfWindowW = Window.Width / 2;
-            float halfWindowH = Window.Height / 2;
-
-            // Top-Left
-            if (mouseX < halfWindowW && mouseY < halfWindowH)
-            {
-                Draw.FillColor = Color.Red;
-                Draw.Square(0, 0, 200);
-            }
-
-            // Top-Right
-            if (mouseX > halfWindowW && mouseY < halfWindowH)
-            {
-                Draw.FillColor = Color.Green;
-                Draw.Square(200, 0, 200);
-            }
-
-            // Bottom-Left
-            if (mouseX < halfWindowW && mouseY > halfWindowH)
-            {
-                Draw.FillColor = Color.Yellow;
-                Draw.Square(0, 200, 200);
-            }
-
-            // Bottom-Right
-            if (mouseX > halfWindowW && mouseY > halfWindowH)
-            {
-                Draw.FillColor = Color.Blue;
-                Draw.Square(200, 200, 200);
-            }
-
-            // Draw lines to separate screen into quadrants
-            Draw.LineSize = 2;
-            Draw.LineSharp(Window.Width / 2, 0, Window.Width / 2, Window.Height);
-            Draw.LineSharp(0, Window.Height / 2, Window.Width, Window.Height / 2);
-            Draw.LineSize = 0;
+            Draw.FillColor = Color.Green;
+            Draw.Square(200, 0, 200);
         }
+
+        // Bottom-Left
+        if (mouseX < halfWindowW && mouseY > halfWindowH)
+        {
+            Draw.FillColor = Color.Yellow;
+            Draw.Square(0, 200, 200);
+        }
+
+        // Bottom-Right
+        if (mouseX > halfWindowW && mouseY > halfWindowH)
+        {
+            Draw.FillColor = Color.Blue;
+            Draw.Square(200, 200, 200);
+        }
+
+        // Draw lines to separate screen into quadrants
+        Draw.LineSize = 2;
+        Draw.LineSharp(Window.Width / 2, 0, Window.Width / 2, Window.Height);
+        Draw.LineSharp(0, Window.Height / 2, Window.Width, Window.Height / 2);
+        Draw.LineSize = 0;
     }
 }
