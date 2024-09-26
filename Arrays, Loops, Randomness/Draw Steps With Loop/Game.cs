7b897ce9a5471902890e4c16e9b072451c55d995
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2024-09-26 17:45:26 -0400

    Add draw steps with loop

---
+++ Arrays, Loops, Randomness/Draw Steps With Loop/Game.cs
@@ -0,0 +1,48 @@
+﻿// Include code libraries you need below (use the namespace).
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace Game10003
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Game
+    {
+        // Place your variables here:
+
+
+        /// <summary>
+        ///     Setup runs once before the game loop begins.
+        /// </summary>
+        public void Setup()
+        {
+            Window.SetTitle("Draw Steps With Loop");
+            Window.SetSize(400, 400);
+            Draw.LineSize = 1;
+        }
+
+        /// <summary>
+        ///     Update runs every frame.
+        /// </summary>
+        public void Update()
+        {
+            // Prepare for drawing
+            Window.ClearBackground(Color.OffWhite);
+            Draw.FillColor = Color.Gray;
+
+            // Loop 10 times (10 steps)
+            for (int i = 0; i < 10; i++)
+            {
+                // Calculate upper-left coordinates
+                int xCoordinate = i * 40;      // space x 40 pixels
+                int yCoordinate = i * 30 + 50; // space y 30 pixels, starting at y of 50
+
+                // Draw using coordinates, and overdraw below screen
+                Draw.Rectangle(xCoordinate, yCoordinate, 40, 400);
+            }
+
+        }
+    }
+}
