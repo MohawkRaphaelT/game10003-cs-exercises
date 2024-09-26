8b7ab0fd461ac904eff927c8500d221e8c0c8747
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2024-09-26 17:34:22 -0400

    m: Update window title and comments

--- Arrays, Loops, Randomness/Draw Shape Line With Loop (Solution)/Game.cs
+++ Arrays, Loops, Randomness/Draw Shape Line With Loop (Solution)/Game.cs
@@ -18,7 +18,7 @@ namespace Game10003
         /// </summary>
         public void Setup()
         {
-            Window.SetTitle("Draw 2D Gradient With Loops");
+            Window.SetTitle("Draw Shape Line With Loop");
             Window.SetSize(400, 400);
         }
 
@@ -31,8 +31,9 @@ namespace Game10003
             Window.ClearBackground(Color.OffWhite);
             Draw.FillColor = Color.Green;
 
-            // 
+            // Set y to fixed coordinate in centre of screen
             float yCoordinate = Window.Height / 2;
+            // Loop over 8 times to compute an X coordinate
             for (int i = 0; i < 8; i++)
             {
                 float xCoordinate = 25 + i * 50;
