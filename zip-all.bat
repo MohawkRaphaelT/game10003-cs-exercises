369b8d87e185a9dd5defcfadccc565b893e69724
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2026-01-28 00:14:59 -0500

    Implement tagged zip archives, remove TODO list

--- zip-all.bat
+++ zip-all.bat
@@ -1,7 +1,8 @@
 :: Run this zip all projects
 
 :: Update MohawkGame2D code
-python zip-projects.py
+python zip-projects.py ""
+::python zip-projects.py " (v1.5)"
 
 :: Read output
 pause
--- zip-projects.py
+++ zip-projects.py
@@ -1,7 +1,9 @@
 import glob
 import shutil
-import os
+import sys
 
+# Get ending tag
+tag = sys.argv[1]
 # Find existing projects
 pattern = "./**/**(*l*)/"
 dir_matches = glob.glob(pattern, recursive=True)
@@ -12,9 +14,9 @@ for dir_match in dir_matches:
     print(f"Processing \"{dir_match}\"")
     # 
     path_input = dir_match
-    dir_name = dir_match # using this keeps folder structure
+    dir_name = dir_match[:-1] # using this keeps folder structure
     #dir_name = dir_match.split("\\")[-2] # get last dir name, assumes tailing \
-    path_output = f"./zip-exports/{dir_name}"
+    path_output = f"./zip-exports/{dir_name}{tag}"
     #print(f"Input : \"{path_input}\".")
     #print(f"Output: \"{path_output}\".")
     shutil.make_archive(path_output, 'zip', path_input)
